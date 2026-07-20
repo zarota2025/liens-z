a38a9921e21dfbb5cd635e72fc67a1ffa3f5954a
zarota2025 <zarozarota@gmail.com>
2026-07-20 20:38:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -83,12 +83,20 @@ buyButtons.forEach(button=>{
         e.preventDefault();
 
         const product = {
+    id: button.dataset.name,
     name: button.dataset.name,
-    price: button.dataset.price,
-    image: button.dataset.image
+    price: Number(button.dataset.price),
+    image: button.dataset.image,
+    quantity: 1
 };
 
-        cart.push(product);
+        const existingProduct = cart.find(item => item.id === product.id);
+
+if (existingProduct) {
+    existingProduct.quantity++;
+} else {
+    cart.push(product);
+}
 
         localStorage.setItem("cart",JSON.stringify(cart));
 
