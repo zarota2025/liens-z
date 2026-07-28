f8af8bdc0b1c864799089bf0e7bb8423ea1836a1
zarota2025 <zarozarota@gmail.com>
2026-07-28 23:21:50 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -14,6 +14,31 @@ const productBadge = document.getElementById("product-badge");
 const productCategory = document.getElementById("product-category");
 const productImage = document.getElementById("product-image");
 let selectedImage = "";
+
+const CLOUD_NAME = "m3wucnpl";
+const UPLOAD_PRESET = "liens-z";
+
+async function uploadImage(file){
+
+    const formData = new FormData();
+
+    formData.append("file", file);
+    formData.append("upload_preset", UPLOAD_PRESET);
+
+    const response = await fetch(
+        `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`,
+        {
+            method: "POST",
+            body: formData
+        }
+    );
+
+    const data = await response.json();
+
+    return data.secure_url;
+
+}
+
 const saveButton = document.getElementById("save-product");
 const productsContainer = document.getElementById("admin-products");
 let editingIndex = -1;
