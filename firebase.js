b2e81d4f74525bae8750ff834679eee7979f2831
zarota2025 <zarozarota@gmail.com>
2026-08-01 14:54:35 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+// Import the functions
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDmzIUU1_ytocfHNs4wnyL8coWVk76Cxz8",
+  authDomain: "liens-z.firebaseapp.com",
+  projectId: "liens-z",
+  storageBucket: "liens-z.firebasestorage.app",
+  messagingSenderId: "356236994136",
+  appId: "1:356236994136:web:c81d3c2a7c54dffd85cc2d",
+  measurementId: "G-91XMYR26ML"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
