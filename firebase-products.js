c48c7cf5a8928f209c33ccac2a1fc7974125938d
zarota2025 <zarozarota@gmail.com>
2026-08-03 22:44:05 +0100

    Update firebase-products.js

--- firebase-products.js
+++ firebase-products.js
@@ -1,1 +1,37 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  doc
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+export async function addProduct(product) {
+    await addDoc(collection(db, "products"), product);
+}
+
+export async function getProducts() {
+    const snapshot = await getDocs(collection(db, "products"));
+
+    let products = [];
+
+    snapshot.forEach((item) => {
+        products.push({
+            id: item.id,
+            ...item.data()
+        });
+    });
+
+    return products;
+}
+
+export async function deleteProduct(id) {
+    await deleteDoc(doc(db, "products", id));
+}
+
+export async function updateProduct(id, product) {
+    await updateDoc(doc(db, "products", id), product);
+}
