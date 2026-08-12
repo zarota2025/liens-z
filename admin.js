cf59fd888f3ec5af332d3cd0b498f15f8bb0106e
zarota2025 <zarozarota@gmail.com>
2026-08-12 20:24:34 +0100

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,123 @@
+// =====================================
+// 🛡️ LIENS Z - ADMIN PANEL
+// PART 1 / 10
+// =====================================
 
+
+// =====================================
+// 🔐 ADMIN LOGIN CHECK
+// =====================================
+
+if (
+    localStorage.getItem("adminLogged") !== "true"
+) {
+
+    location.href = "admin-login.html";
+
+}
+
+
+// =====================================
+// 📦 FIREBASE PRODUCTS
+// =====================================
+
+import {
+    addProduct,
+    getProducts,
+    deleteProduct as deleteFirebaseProduct,
+    updateProduct
+} from "./firebase-products.js";
+
+
+// =====================================
+// 🔥 FIREBASE FIRESTORE
+// =====================================
+
+import {
+    db,
+    collection,
+    getDocs
+} from "./firebase.js";
+
+
+// =====================================
+// 📦 GLOBAL VARIABLES
+// =====================================
+
+let adminProducts = [];
+
+let selectedImage = "";
+
+let editingIndex = -1;
+
+let salesChart = null;
+
+
+// =====================================
+// 📝 PRODUCT FORM ELEMENTS
+// =====================================
+
+const productName =
+    document.getElementById(
+        "product-name"
+    );
+
+
+const productPrice =
+    document.getElementById(
+        "product-price"
+    );
+
+
+const productOldPrice =
+    document.getElementById(
+        "product-old-price"
+    );
+
+
+const productDiscount =
+    document.getElementById(
+        "product-discount"
+    );
+
+
+const productBadge =
+    document.getElementById(
+        "product-badge"
+    );
+
+
+const productCategory =
+    document.getElementById(
+        "product-category"
+    );
+
+
+const productImage =
+    document.getElementById(
+        "product-image"
+    );
+
+
+const saveButton =
+    document.getElementById(
+        "save-product"
+    );
+
+
+const productsContainer =
+    document.getElementById(
+        "admin-products"
+    );
+
+
+// =====================================
+// ☁️ CLOUDINARY
+// =====================================
+
+const CLOUD_NAME =
+    "m3wucnpl";
+
+
+const UPLOAD_PRESET =
+    "liens-z";
